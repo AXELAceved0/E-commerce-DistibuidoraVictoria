6211f0d63740095f33edde743acff0f8b6e975a6
Axel Acevedo <axelacevedo021@gmail.com>
2024-12-08 16:48:12 -0300

    login

--- src/components/header/pages/LoginPages.jsx
+++ src/components/header/pages/LoginPages.jsx
@@ -1,16 +1,38 @@
 import styles from './LoginPages.module.css'
-import fondo from '../../../assets/alfareros.png'
+import logo from '../../../assets/iconoVictoria.png'
+import { useEffect } from 'react'
 
 
 const LoginPages = () => {
+    useEffect(() => {
+        document.body.classList.add('noBackground'); // Añade la clase al cargar la página
+
+        return () => {
+            document.body.classList.remove('noBackground');
+        }
+    }, [])
     return (
         <div className={styles.container}>
-            <img className={styles.img} src={fondo} alt="fondo" />
-            <div>
-            <h1>Iniciar Sesión</h1>
-            </div>
-            <form>
-                
+            <form className={styles.form}>
+                <div className={styles.containerForm}>
+                <div className={styles.containerTitle}>
+                    <img src={logo} alt="logo" width="135px" height="135px" />
+                    <h1>Distribuidora Victoria</h1>
+                </div>
+                <div className={styles.containerLogin}>
+                    <label htmlFor="email">
+                        <i className="bi bi-person-circle">
+                        </i>
+                        <input type="email" placeholder=' Email' id='email' required />
+                    </label>
+                    <label htmlFor="passwords">
+                        <i className="bi bi-lock-fill"></i>
+                        <input type="passwords" id='passwords' placeholder=' Contraseña' required />
+                    </label>
+                    <button>Iniciar Sesión</button>
+
+                </div>
+                </div>
             </form>
         </div>
     )
